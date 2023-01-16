a350d134166f8591967bd9a02eeac7fcb8d63b44
MelihEmreGuler <melihemreguler@hotmail.com>
2023-01-16 03:16:19 +0300

    json file paths fixed

--- dataloaders/dataloaders.go
+++ dataloaders/dataloaders.go
@@ -3,30 +3,39 @@ package dataloaders
 import (
 	"encoding/json"
 
-	. "../models"
-	util "../utils"
+	"github.com/MelihEmreGuler/user-information-system/models"
+	"github.com/MelihEmreGuler/user-information-system/utils"
 )
 
-func LoadUsers() []User {
-	bytes, _ := util.ReadFile("../json/user.json")
-
-	var data []User
+func LoadUsers() []models.User {
+	bytes, err := utils.ReadFile("json/user.json")
+	if err != nil {
+		panic(err)
+	}
+	//fmt.Println("LoadUsers:\n" + bytes)
+	var data []models.User
 	json.Unmarshal([]byte(bytes), &data)
 
 	return data
 }
-func LoadInterests() []Interest {
-	bytes, _ := util.ReadFile("../json/interests.json")
-
-	var data []Interest
+func LoadInterests() []models.Interest {
+	bytes, err := utils.ReadFile("json/interests.json")
+	if err != nil {
+		panic(err)
+	}
+	//fmt.Println("LoadInterests:\n" + bytes)
+	var data []models.Interest
 	json.Unmarshal([]byte(bytes), &data)
 
 	return data
 }
-func LoadinterestsMappings() []InterestMapping {
-	bytes, _ := util.ReadFile("../json/userInterestsMappings.json")
-
-	var data []InterestMapping
+func LoadinterestsMappings() []models.InterestMapping {
+	bytes, err := utils.ReadFile("json/userInterestsMappings.json")
+	if err != nil {
+		panic(err)
+	}
+	//fmt.Println("LoadinterestsMappings:\n" + bytes)
+	var data []models.InterestMapping
 	json.Unmarshal([]byte(bytes), &data)
 
 	return data
