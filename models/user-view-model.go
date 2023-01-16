ca7468ccd50407a5f9cba8226de3596e76433e74
MelihEmreGuler <melihemreguler@hotmail.com>
2023-01-16 03:21:31 +0300

    comment added

--- models/user-view-model.go
+++ models/user-view-model.go
@@ -5,5 +5,5 @@ type UserViewModel struct {
 	Users []User
 }
 
-//UserViewModel nesnesi üzerinden hem sayfa bilgilerini hem de kullanıcı bilgilerini tutuyoruz.
+//UserViewModel tipi üzerinden hem sayfa bilgilerini hem de kullanıcı bilgilerini tutuyoruz.
 //Bu sayede kullanıcı bilgilerini ve sayfa bilgilerini tek bir struct üzerinden yönetebiliyoruz.
